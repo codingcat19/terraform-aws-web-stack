40ff502c104cdd06b30d6b6a6bb81b641e57565b
Sahil Shivalkar <sahil@Sahils-MacBook-Air.local>
2026-01-05 21:53:12 +0530

    Fixed issues

---
+++ modules/ec2/main.tf
@@ -0,0 +1,53 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical's official AWS account ID
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_security_group" "web_sg" {
+  name   = "${var.project_name}-web-sg"
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web_server" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t3.micro"
+  subnet_id              = var.public_subnet_id
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              apt-get update -y
+              apt-get install -y nginx
+              systemctl start nginx
+              systemctl enable nginx
+              echo "<h1>Hello from Ubuntu on $(hostname -f)</h1>" > /var/www/html/index.html
+              EOF
+
+  tags = {
+    Name = "${var.project_name}-ubuntu-server"
+  }
+}
---
+++ modules/vpc/variables.tf
@@ -0,0 +1,3 @@
+variable "vpc_cidr" {}
+variable "project_name" {}
+variable "aws_region" {}
