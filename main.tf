aa0b9dba76d25be15a36b36d3e3452a78ba59ce1
Sahil Shivalkar <sahil@Sahils-MacBook-Air.local>
2026-01-05 21:49:13 +0530

    Fixed typo

--- main.tf
+++ main.tf
@@ -1,1 +1,13 @@
-hello
+module "vpc" {
+  source = "./modules/vpc"
+  project_name = var.project_name
+  vpc_cidr     = var.vpc_cidr
+  aws_region   = var.aws_region
+}
+
+module "ec2" {
+  source = "./modules/ec2"
+  project_name = var.project_name
+  vpc_id = module.vpc.vpc_id
+  public_subnet_id = module.vpc.public_subnet_id
+}
